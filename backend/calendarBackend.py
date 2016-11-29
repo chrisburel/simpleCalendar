49089a2a759514eb0aef9d9771515d254a6e0095
Chris Burel <chrisburel@gmail.com>
2016-11-29 11:52:38 -0800

    Add CORS call to allow cross-origin resource sharing for all domains.

--- backend/calendarBackend.py
+++ backend/calendarBackend.py
@@ -2,9 +2,13 @@
 
 from flask import Flask
 from flask.ext.restful import Api
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+# Allow cross-origin resource sharing for all domains on all routes.  In a
+# real-world app this would be much more restrictive.
+CORS(app)
 
 from calendardb.resources import EventListResource
 
